6ea2ddbf05e7992f281793f621ffe22a90266344
Alex Marandon <alex.marandon@weatherforce.org>
2020-05-27 10:21:16 +0200

    Add nearest cities endpoint

--- lib/chercheville/web.ex
+++ lib/chercheville/web.ex
@@ -12,11 +12,33 @@ defmodule ChercheVille.Web do
       %{"q" => q} ->
         results = Search.text(q)
         resp_data = post_process_results(results)
-        resp_body = Jason.encode!(resp_data)
-        send_resp(conn, 200, resp_body)
+        send_json(conn, resp_data)
       _ ->
-        send_resp(conn, 400, "Bad request: missing query string parameter `q`")
+        send_resp(conn, 400, "Bad request: missing query string parameter `q`.")
     end
+  end
+
+  get "/cities/nearest" do
+    conn = fetch_query_params(conn)
+    with %{"lat" => latitude, "lon" => longitude} <- conn.params,
+         {latitude, _} <- Float.parse(latitude),
+         {longitude, _} <- Float.parse(longitude)
+    do
+        results = Search.coordinates(latitude, longitude, country_code: country_code(conn.params))
+        resp_data = results
+                    |> post_process_results
+        send_json(conn, resp_data)
+    else
+      _ -> send_resp(conn, 400, "Bad request: invalid coordinates. Expecting numbers for `lat` and `lon` parameters.")
+    end
+  end
+
+  defp send_json(conn, resp_data, status_code \\ 200) do
+    resp_body = Jason.encode!(resp_data)
+
+    conn
+    |> put_resp_header("content-type", "application/json; charset=utf-8")
+    |> send_resp(status_code, resp_body)
   end
 
   defp post_process_results(results) do
@@ -35,7 +57,25 @@ defmodule ChercheVille.Web do
     ])
   end
 
+  defp country_code(query_string) do
+    default_country_code = Application.get_env(:chercheville, :default_country_code)
+    Map.get(query_string, "country_code", default_country_code)
+  end
+
   match _ do
-    send_resp(conn, 404, "oops")
+    table_of_contents = %{
+      "message" => "Not found",
+      "endpoints" => %{
+        "cities" => %{
+          "url" => "/cities/?q={search_string}",
+          "description" => "City search service. Provide search string as a query string parameter `q`."
+        },
+        "nearest_cities" => %{
+          "url" => "/cities/nearest?lat={latitude}&lon={longitude}[&country_code={country_code}]",
+          "description" => "Cities located nearest to requested coordinates. Optionally filter by country code."
+        }
+      }
+    }
+    send_json(conn, table_of_contents, 404)
   end
 end
--- test/web_test.exs
+++ test/web_test.exs
@@ -8,41 +8,86 @@ defmodule MyPlugTest do
   setup do
     :ok = Ecto.Adapters.SQL.Sandbox.checkout(ChercheVille.Repo)
     Ecto.Adapters.SQL.Sandbox.mode(ChercheVille.Repo, {:shared, self()})
+    insert_city(123_930_305, "Toulouse")
+    :ok
   end
 
-  describe "city search" do
+  describe "text search" do
 
     test "with valid query string" do
-      insert_city(123_930_305, "Toulouse")
       # Create a test connection
-      conn = conn(:get, "/cities?q=Toulouse")
-
-      # Invoke the plug
-      conn = ChercheVille.Web.call(conn, @opts)
+      conn = get("/cities?q=Toulouse")
 
       # Assert the response and status
-      assert conn.state == :sent
       assert conn.status == 200
 
-      response_body = conn.resp_body
-      response_data = Jason.decode!(response_body)
+      response_data = decode_body(conn)
       top_result = Enum.at(response_data, 0)
       assert Map.get(top_result, "name") == "Toulouse"
+
+      assert get_resp_header(conn, "content-type") == ["application/json; charset=utf-8"]
     end
 
     test "with invalid query string" do
-      insert_city(123_930_305, "Toulouse")
       # Create a test connection
-      conn = conn(:get, "/cities?foo=42")
-
-      # Invoke the plug
-      conn = ChercheVille.Web.call(conn, @opts)
+      conn = get("/cities?foo=42")
 
       # Assert the response and status
-      assert conn.state == :sent
       assert conn.status == 400
       assert conn.resp_body =~ "missing query string param"
     end
 
+  end
+
+  describe "not found" do
+    test "should return endpoints" do
+      conn = get("/")
+      response_data = decode_body(conn)
+      assert Map.has_key?(response_data["endpoints"]["cities"], "url")
+      assert Map.has_key?(response_data["endpoints"]["nearest_cities"], "url")
+    end
+  end
+
+  describe "nearest search" do
+
+    test "with valid coordinates" do
+      conn = get("/cities/nearest?lat=43.1&lon=0.96")
+
+      # Assert the response and status
+      assert conn.status == 200
+
+      response_data = decode_body(conn)
+      top_result = Enum.at(response_data, 0)
+      assert Map.get(top_result, "name") == "Toulouse"
+    end
+
+    test "with missing parameter" do
+      conn = get("/cities/nearest?lat=43.1")
+
+      # Assert the response and status
+      assert conn.status == 400
+      assert conn.resp_body =~ "invalid coordinates"
+    end
+
+    test "with invalid coordinate" do
+      conn = get("/cities/nearest?lat=43.1&lon=foo")
+
+      # Assert the response and status
+      assert conn.status == 400
+      assert conn.resp_body =~ "invalid coordinates"
+    end
+  end
+
+  # Helper functions
+  defp get(url) do
+    # Create a test connection
+    conn = conn(:get, url)
+
+    # Invoke the plug
+    ChercheVille.Web.call(conn, @opts)
+  end
+
+  defp decode_body(conn) do
+    conn.resp_body |> Jason.decode!
   end
 end
