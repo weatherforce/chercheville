5fc2aca245f90f5017e990c2283c9b06d1912ab9
Alex Marandon <alex.marandon@weatherforce.org>
2020-05-27 10:41:47 +0200

    Update version number

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule ChercheVille.Mixfile do
     [
       app: :chercheville,
       description: "Service allowing to search cities based on data from GeoNames",
-      version: "0.2.0",
+      version: "0.3.0",
       elixir: "~> 1.6",
       source_url: @repo_url,
       start_permanent: Mix.env() == :prod,
